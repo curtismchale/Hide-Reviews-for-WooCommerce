d413397eb45f083774497223178bd127c366e6bc
Curtis McHale <curtis@curtismchale.ca>
2014-08-29 16:07:18 -0700

    - testing the removal of the reviews tab
    This tests to make sure that we remove the reviews tab if we want it
    gon, and that we don't remove it if the internal options tell us not to
    remove it.

--- tests/test-base_plugin.php
+++ tests/test-base_plugin.php
@@ -19,5 +19,47 @@ class TestBasePlugin extends WP_UnitTestCase {
 		$this->assertFalse( null == $this->plugin, 'testPluginActive says our plugin is not loaded' );
 	}
 
+	/**
+	 * Makes sure that the reviews tab was removed from the array
+	 *
+	 * @since 1.0
+	 * @author SFNdesign, Curtis McHale
+	 */
+	function testRemoveReviewsTab(){
+		update_option( 'woocommerce_enable_review_rating', 'no' );
+
+		$tabs = array(
+			'reviews' => 'something',
+			'else'    => 'another value',
+		);
+
+		$returned_tabs = $this->plugin->kill_wc_reviews_tab( $tabs );
+
+		$this->assertArrayNotHasKey( 'reviews', $returned_tabs, 'Reviews may not have been removed from the tabs' );
+
+		delete_option( 'woocommerce_enable_review_rating' );
+	}
+
+	/**
+	 * Makes sure that the reviews tab was NOT removed if the option isn't set for it to be removed
+	 *
+	 * @since 1.0
+	 * @author SFNdesign, Curtis McHale
+	 */
+	function testNotRemoveReviewsTab(){
+		update_option( 'woocommerce_enable_review_rating', 'yes' );
+
+		$tabs = array(
+			'reviews' => 'something',
+			'else'    => 'another value',
+		);
+
+		$returned_tabs = $this->plugin->kill_wc_reviews_tab( $tabs );
+
+		$this->assertArrayHasKey( 'reviews', $returned_tabs, 'Reviews tab was removed when we expected it not to be removed' );
+
+		delete_option( 'woocommerce_enable_review_rating' );
+	}
+
 }
 
